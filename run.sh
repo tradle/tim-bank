0f356086aa8437b1b204d37979b1e79c823dbcea
Mark Vayngrib <mark@tradle.io>
2016-02-15 15:01:59 -0500

    simplify run.sh

--- run.sh
+++ run.sh
@@ -7,24 +7,28 @@ if [[ "$NODE_ENV" == "development" || "$NODE_ENV" == "production" ]]; then
   fi
 fi
 
-mkdir -p "./storage/logs"
-LOG_PATH="./storage/logs/debug-$(date +%s).log"
-
 case "$NODE_ENV" in
   development)
-    node banks.js ./conf/conf.json -s --public -b $PROVIDERS > LOG_PATH
     ;;
   production)
-    DEBUG="" node banks.js ./conf/conf.json -s --public -b $PROVIDERS > LOG_PATH
+    DEBUG=""
     ;;
   test)
     npm test
+    exit 0
     ;;
   "")
     npm test
+    exit 0
     ;;
   *)
     echo "Unsupported value of \$NODE_ENV=$NODE_ENV"
     exit 1
     ;;
 esac
+
+mkdir -p "./$STORAGE_PATH/logs"
+LOG_PATH="./$STORAGE_PATH/logs/debug-$(date +%s).log"
+DEBUG_FD=3
+
+node banks.js ./conf/conf.json -s --public -b "$PROVIDERS" -d "$STORAGE_PATH" 3>"$LOG_PATH"
